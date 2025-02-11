63c1e861da7e138c2d7afca16ae85e2d7d42cf37
dani007200964 <dani007200964@gmail.com>
2025-02-11 19:57:24 +0100

    First commit
    The project already forked for years, I think it's time to share it. Docuemntation still needed.

--- extras/System.cpp
+++ extras/System.cpp
@@ -1,6 +1,16 @@
+/*
+ * Created on April 08 2023
+ *
+ * Copyright (c) 2020 - Daniel Hajnal
+ * hajnal.daniel96@gmail.com
+ * This file is part of the Shellminator project.
+ * Modified 2023.04.08
+*/
+
+/*
 MIT License
 
-Copyright (c) 2023 Daniel Hajnal
+Copyright (c) 2020 Daniel Hajnal
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -19,3 +29,14 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+*/
+
+#include "System.h"
+
+unsigned long millis(){
+	return (unsigned long)HAL_GetTick();
+}
+
+void delay( uint32_t x ){
+    HAL_Delay( x );
+}
